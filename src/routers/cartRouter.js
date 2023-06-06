29e0bf3e5db4a783ed98a5267c5d2eca337548c3
chivi <paulofr016@gmail.com>
2023-06-06 12:45:59 -0300

    cambios pre entrega 2

--- src/routers/cartRouter.js
+++ src/routers/cartRouter.js
@@ -18,60 +18,28 @@ router.post('/', async (req, res) => {
 
 // Endpoint para obtener un carrito por ID
 router.get('/:cartId', async (req, res) => {
-  try {
     const { cartId } = req.params;
-    
-    const cart = await cartModel.aggregate([
-      { $match: { _id: mongoose.Types.ObjectId(cartId) } },
-      { $unwind: "$products" },
-      {
-        $group: {
-          _id: "$products.product",
-          totalQuantity: { $sum: "$products.quantity" }
-        }
-      },
-      {
-        $lookup: {
-          from: "products",
-          localField: "_id",
-          foreignField: "_id",
-          as: "product"
-        }
-      },
-      { $unwind: "$product" },
-      {
-        $project: {
-          _id: "$product._id",
-          name: "$product.name",
-          quantity: "$totalQuantity"
-        }
-      }
-    ]);
-    
-    res.render('carts', { products: cart });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+    try {
+      const cart = await cartManager.getCartById(cartId);
+  
+      res.render('carts', { cart });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }}
+);
 
 
 
 
 // Endpoint para agregar un producto a un carrito
-router.post('/products', async (req, res) => {
+router.post('/:cartId/products/:productId', async (req, res) => {
   try {
-    const { cartId, productId, quantity } = req.body;
+    const { cartId, productId } = req.params;
+    const { quantity } = req.body;
     
-    let updatedCart;
+    const updatedCart = await cartManager.addProductToCart(cartId, productId, quantity);
     
-    if (!cartId) {
-      // Si no hay un carrito, crear un nuevo carrito
-      const newCart = await cartManager.createCart([{ product: productId, quantity }]);
-      updatedCart = newCart;
-    } else {
-      // Si se proporciona un ID de carrito, agregar productos al carrito existente
-      updatedCart = await cartManager.addProductToCart(cartId, productId, quantity);
-    }
+    res.json(updatedCart);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
